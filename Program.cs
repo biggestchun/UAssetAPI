cd5c49581f700a3dc0d3b9e2f89bcf427b258bda
biggestchun <85227635+biggestchun@users.noreply.github.com>
2025-01-22 22:46:18 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -6,22 +6,5 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World from UAssetAPI Example!");
-        
-        try
-        {
-            // Create a new UAsset to verify the library reference works
-            var asset = new UAsset();
-            Console.WriteLine("Successfully created UAsset instance");
-            
-            // Show the version of the UAsset API being used
-            Console.WriteLine($"UAssetAPI Version: {typeof(UAsset).Assembly.GetName().Version}");
-        }
-        catch (Exception ex)
-        {
-            Console.WriteLine($"Error initializing UAsset: {ex.Message}");
-        }
-        
-        Console.WriteLine("\nPress any key to exit...");
-        Console.ReadKey();
     }
 }
